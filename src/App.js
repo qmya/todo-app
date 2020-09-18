0d036019e2799e2454ebf3faaccc5e25a4e3118f
qmya <qandeelgul90@gmail.com>
2020-09-18 12:11:32 -0400

    pased the todo list to the app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 
 function App() {
   const[ todos, setTodos ] = useState([]);
+
+  //to add the todo to the todos array we need another function
+  function addTodo(todo){
+    setTodos([todo, ...todos]);
+  }
   return (
     <div className="App">
      <h1> React Todo App </h1>
