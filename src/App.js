3d5c904ed5bec13174e28dbce1ecef2b91635691
qmya <qandeelgul90@gmail.com>
2020-09-18 11:43:53 -0400

    added setState to update the current state of app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { setState, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[ todos, setTodos ] = useState([]);
   return (
     <div className="App">
-     <h1> Todo App </h1>
+     <h1> React Todo App </h1>
     </div>
   );
 }
