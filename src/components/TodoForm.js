20f22c7aba73aff220c00aee2c7be7148762fe3e
qmya <qandeelgul90@gmail.com>
2020-09-18 12:10:58 -0400

    created todo form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,26 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 function TodoForm() {
+    const [ todo, setTodo ] = useState({
+        id="",
+        task="",
+        completed=False
+    })
+    //event handling function
+    function handleTaskInputChange(e) {
+        setTodo({ ...todo, task: e.target.value })
+    }
   return (
     <div>
-     
+     <form>
+         <input
+            name="task"
+            type="text"
+            value={todo.task}
+            onChange= {handleTaskInputChange}
+         />
+         <button/>
+     </form>
     </div>
   );
 }
